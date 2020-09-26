a5a428c057163296b41d50ec04a724d1b15e681e
Taylor Booth <taylorkbooth@gmail.com>
2020-09-26 04:28:14 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -21,7 +21,17 @@ class Waiter
   def meals
   Meal.all.select do |meal|
     meal.waiter == self
+    end
   end
-end
+  
+  def best_tipper
+  best_tipped_meal = meals.max do |meal_a, meal_b|
+    meal_a.tip <=> meal_b.tip
+    end
+ 
+    best_tipped_meal.customer
+  end
+ 
+ 
  
 end
