7438971b5873e211bf1450eef4741f2eee4a216e
Taylor Booth <taylorkbooth@gmail.com>
2020-09-26 04:13:34 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -2,8 +2,13 @@ class Waiter
 
   attr_accessor :name, :yrs_experience
 
-  def initialize(name, yrs_experience)
+    @@all = []
     
+  def initialize(name, yrs_experience)
+    @name = name
+    @yrs_experience = yrs_experience
+    @@all << self
+  end
 
 
 end
